c241d9161a5ca910d7178a5e290838acc232cd70
slicko02 <122498753+slicko02@users.noreply.github.com>
2023-04-19 19:44:19 -0500

    Added Music and Sound Control Functions

--- js/index.js
+++ js/index.js
@@ -3,6 +3,42 @@ const wallpaper2 = document.getElementById("yellow");
 const wallpaper3 = document.getElementById("green");
 const wallpaper4 = document.getElementById("blue");
 const wallpaper5 = document.getElementById("rand");
+let homeMusic = new Audio("./img/TechTalk.mp3");
+var vol =.9;
+var vol2 = .5;
+
+homeMusic.autoplay = true;
+homeMusic.loop = true;
+
+function playMusic() { 
+  homeMusic.play();
+};
+
+function pauseMusic() { 
+  homeMusic.pause();
+};
+
+function lowerMusic() {  
+  vol2=.3;
+  homeMusic.volume = vol;
+  if(vol>.1){
+  vol = vol -.1;
+  }
+  else{
+    vol =vol;
+  }  
+};
+
+function higherMusic() { 
+vol = .9;
+  homeMusic.volume = vol2;
+  if(vol<1){
+  vol2= vol2 +.1;
+  }
+  else{
+    vol2 = vol2;
+  }
+};
 
 wallpaper1.addEventListener("click", () =>{
 document.body.style.background = 'pink';
